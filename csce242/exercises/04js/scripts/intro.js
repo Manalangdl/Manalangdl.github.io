c38b634d1f851e4d8308afbb57e28e75f29f2c3f
Lindsay Manalang <manalang@email.sc.edu>
2026-09-10 12:44:52 -0400

    Class exercise on ball animation

--- csce242/exercises/04js/scripts/intro.js
+++ csce242/exercises/04js/scripts/intro.js
@@ -3,7 +3,23 @@
 //     console.log("I did it! The button was clicked!");
 // }
 
+
+// shows a message when the buttons ic clicked
 const btnClick = document.getElementById("btn-show-message").onclick = (e) =>{
     document.getElementById("p-message").innerHTML = "Hello World";
     e.target.innerHTML = "done!";
+};
+
+// styles the link when it's clicked
+document.getElementById("link").onclick = (e) =>{
+    e.preventDefault(); //don't go to link's destination
+    e.target.classList.add("cool-link");
+    // console.log("you clicked");
+};
+
+
+// when button clicked make ball bounce
+document.getElementById("btn-bounce").onclick = (e) =>{
+    document.getElementById("ball").classList.toggle("bouncing-ball");
+    // console.log("hi"); //checks for err
 }
