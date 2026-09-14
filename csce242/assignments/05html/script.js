f6be92e3279bbf3148d5bcfa8e7c91a79bda80be
Lindsay Manalang <manalang@email.sc.edu>
2026-09-13 23:29:22 -0400

    Finished column 3 with the sticker and img

--- csce242/assignments/05html/script.js
+++ csce242/assignments/05html/script.js
@@ -1,13 +1,20 @@
+// first column
 document.getElementById("box1").onclick = (e) => {
-
 
     document.getElementById("bubble").classList.add("active");
 }
 
+// second column
 document.getElementById("select").onclick = (e) => {
 
     if (e.target.value != "") {
         document.getElementById("select-text").classList.remove("hidden");
         document.getElementById("select-text").innerHTML = e.target.value + ": Nice Choice!";
     }
+}
+
+// third column
+document.getElementById("img-btn").onclick = (e) => {
+
+    document.getElementById("heart").classList.remove("hidden")
 }
