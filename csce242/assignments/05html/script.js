82b82a4865998078e30a3079ad72630d707a185a
Lindsay Manalang <manalang@email.sc.edu>
2026-09-13 23:20:47 -0400

    Finished the middle column and working on the final third column.

--- csce242/assignments/05html/script.js
+++ csce242/assignments/05html/script.js
@@ -3,3 +3,11 @@ document.getElementById("box1").onclick = (e) => {
 
     document.getElementById("bubble").classList.add("active");
 }
+
+document.getElementById("select").onclick = (e) => {
+
+    if (e.target.value != "") {
+        document.getElementById("select-text").classList.remove("hidden");
+        document.getElementById("select-text").innerHTML = e.target.value + ": Nice Choice!";
+    }
+}
